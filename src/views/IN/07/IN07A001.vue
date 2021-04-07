3bfc4ae688102c620df02108b18947c8a35d0cb1
zun5761(안효주) <zun5761@gmail.com>
2021-04-07 15:02:05 +0900

    vue-slider-component 라이브러리 추가

---
+++ src/views/IN/07/IN07A001.vue
@@ -0,0 +1,27 @@
+<template>
+  <kb-page page-title="가상투자">
+    <kb-page-body>
+      <div class="section">
+        <kb-slider
+          v-model="sliderVal"
+          :step="5"
+          :marks="[0, 20, 40, 60, 80, 100]"
+        />
+        {{sliderVal}}
+      </div>
+    </kb-page-body>
+  </kb-page>
+</template>
+<script>
+
+export default {
+  name: 'IN07A001',
+  data() {
+    return {
+      sliderVal: 20,
+    };
+  },
+  methods: {
+  },
+};
+</script>
---
+++ src/views/IN/07/IN07A002.vue
@@ -0,0 +1,20 @@
+<template>
+  <kb-page page-title="자산비중변경">
+    <kb-page-body>
+      <div class="section">
+      </div>
+    </kb-page-body>
+  </kb-page>
+</template>
+<script>
+
+export default {
+  name: 'IN07A002',
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
