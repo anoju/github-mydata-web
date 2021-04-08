f61d1dea204410112340a24afe5be8610015a700
zun5761(안효주) <zun5761@gmail.com>
2021-04-08 15:02:52 +0900

    작업중

---
+++ src/views/IN/07/IN07A003_tip2.vue
@@ -0,0 +1,18 @@
+<template>
+  <kb-pop
+    tooltip
+    title="해외주식"
+  >
+    <kb-pop-body>
+      <div class="section">
+        선진국 및 이머징 국가의 기업들에 골고루 투자하고 싶으신가요? 그렇다면 해외 주식을 매수하세요.<br>
+        수익률 산정을 위한 벤치마크는 MSCI ACWI 지수(달러기준)를 사용합니다.
+      </div>
+    </kb-pop-body>
+  </kb-pop>
+</template>
+<script>
+export default {
+  name: 'IN07A003_tip1',
+};
+</script>
