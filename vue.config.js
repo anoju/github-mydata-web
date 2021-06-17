c5f034cba53409706033896c99fadfabb615c102
안효주 <zun5761@gmail.com>
2021-06-17 16:40:38 +0900

    수정

--- vue.config.js
+++ vue.config.js
@@ -10,17 +10,6 @@ module.exports = {
   },
   devServer: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        pathRewrite: { '^/api': '' },
-      },
-      '/kings': {
-        target: 'http://14.35.234.7:8081',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/kings': '',
-        },
-      },
     },
     port: 8082,
   },
